022d743cf3d9d004adcfd38d131ec4a4dc61a0e0
thiensuxlawst <thiensuxlawst@gmail.com>
2017-04-02 13:31:06 +0700

    Lần đầu làm chuyện ấy

---
+++ app/src/main/java/com/example/administrator/demo_sqlite1/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.administrator.demo_sqlite1;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+    Button btnCDB;
+    final String DATABASE_NAME="NhanVienDB.sqlite";
+    SQLiteDatabase database;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        btnCDB=(Button) findViewById(R.id.btnTaoDB);
+        btnCDB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                database = Database.initDatabase(MainActivity.this,DATABASE_NAME);
+                Cursor cursor=database.rawQuery("select * from NhanVien",null);
+                cursor.moveToFirst();
+
+                Toast.makeText(MainActivity.this, cursor.getString(1), Toast.LENGTH_LONG).show();
+            }
+        });
+//
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
